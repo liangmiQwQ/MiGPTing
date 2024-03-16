3908a7cc531eb184f4ea11a3876b36821ec6f1f2
liangmiQwQ <mi_yyds@126.com>
2024-03-16 18:27:24 +0800

    feat: sliderBar introduce

--- app/_components/SideBar.tsx
+++ app/_components/SideBar.tsx
@@ -1,10 +1,24 @@
-import { Button, Flex } from "@radix-ui/themes";
+import { Avatar, Flex, Heading, Text } from "@radix-ui/themes";
 import React from "react";
 
 export default function SideBar() {
   return (
-    <Flex className="w-3/12 bg-accent-700 h-full">
-      111<Button>111</Button>
+    <Flex
+      className="lg:w-3/12 sm:w-1/3 w-4/5 bg-slate-100 h-full p-7"
+      direction={"column"}
+      gap="3"
+    >
+      <Flex direction={"column"} gap={"2"}>
+        <Flex align={"center"} justify={"between"}>
+          <Heading className="">{process.env.name}</Heading>
+          <Avatar
+            fallback={process.env.name?.charAt(0) as string}
+            size={"2"}
+            src="../logo.svg"
+          ></Avatar>
+        </Flex>
+        <Text className="text-sm">{process.env.description}</Text>
+      </Flex>
     </Flex>
   );
 }
