948e60cbacdf6b6d3702647f2df56c3f02a05b26
liangmiQwQ <mi_yyds@126.com>
2024-03-19 20:30:11 +0800

    chore:  use turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverMinification: true,
+    useDeploymentId: true,
+    webpackBuildWorker: true,
+  },
+}
 
 export default nextConfig
