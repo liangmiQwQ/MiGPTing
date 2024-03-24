53c4fb2d28b87f89bb74a5be5e84a26ee7ff3216
liangmiQwQ <mi_yyds@126.com>
2024-03-24 15:02:24 +0800

    feat: remove error i18n in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ import { getTranslations } from 'next-intl/server'
 
 export default async function page() {
   const t = await getTranslations()
-  return <div>{t('mask')}111</div>
+  return <div>Noting</div>
 }
